31f426202d3b78d7d3fa9d9f82a3e77138ad7d4a
Scott Cormier <chicormier@gmail.com>
2024-01-06 15:02:39 -0600

    forms

--- features/campsites/RenderCampsite.js
+++ features/campsites/RenderCampsite.js
@@ -4,35 +4,38 @@ import { baseUrl } from '../../shared/baseUrl';
 
 const RenderCampsite = (props) => {
   const { campsite } = props;
+
   if (campsite) {
     return (
       <Card containerStyle={styles.cardContainer}>
         <Card.Image source={{ uri: baseUrl + campsite.image }}>
           <View style={{ justifyContent: 'center', flex: 1 }}>
-            <Text
-              style={{
-                color: 'white',
-                textAlign: 'center',
-                fontSize: 20,
-              }}
-            >
-              {campsite.name}
-            </Text>
+            <Text style={styles.cardText}>{campsite.name}</Text>
           </View>
         </Card.Image>
         <Text style={{ margin: 20 }}>{campsite.description}</Text>
-        <Icon
-          name={props.isFavorite ? 'heart' : 'heart-o'}
-          type='font-awesome'
-          color='#f50'
-          raised
-          reverse
-          onPress={() =>
-            props.isFavorite
-              ? console.log('Already set as a favorite')
-              : props.markFavorite()
-          }
-        />
+        <View style={styles.cardRow}>
+          <Icon
+            name={props.isFavorite ? 'heart' : 'heart-o'}
+            type='font-awesome'
+            color='#f50'
+            raised
+            reverse
+            onPress={() =>
+              props.isFavorite
+                ? console.log('Already set as a favorite')
+                : props.markFavorite()
+            }
+          />
+          <Icon
+            name={props.isFavorite ? 'pencil' : 'pencil'}
+            type='font-awesome'
+            color='#5637DD'
+            raised
+            reverse
+            onPress={() => props.onShowModal()}
+          />
+        </View>
       </Card>
     );
   }
@@ -44,6 +47,23 @@ const styles = StyleSheet.create({
     padding: 0,
     margin: 0,
     marginBottom: 20,
+  },
+
+  cardRow: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    flexDirection: 'row',
+    margin: 20,
+  },
+
+  cardText: {
+    textShadowColor: 'rgba(0, 0, 0, 1)',
+    textShadowOffset: { width: -1, height: 1 },
+    textShadowRadius: 20,
+    textAlign: 'center',
+    color: 'white',
+    fontSize: 20,
   },
 });
 
