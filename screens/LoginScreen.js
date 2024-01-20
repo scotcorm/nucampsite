5d61a35d6623ede6c0596d20158c9faf4c476cbc
Scott Cormier <chicormier@gmail.com>
2024-01-20 14:44:45 -0600

    changes01202024

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -6,6 +6,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import * as ImagePicker from 'expo-image-picker';
 import { baseUrl } from '../shared/baseUrl';
 import logo from '../assets/images/logo.png';
+import * as ImageManipulator from 'expo-image-manipulator';
 
 const LoginTab = ({ navigation }) => {
   const [username, setUsername] = useState('');
@@ -147,10 +148,36 @@ const RegisterTab = () => {
       });
       if (capturedImage.assets) {
         console.log(capturedImage.assets[0]);
-        setImageUrl(capturedImage.assets[0].uri);
+        processImage(capturedImage.assets[0].uri);
       }
     }
   };
+
+  const getImageFromGallery = async () => {
+    const galleryPermission =
+      await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (galleryPermission.status === 'granted') {
+      const capturedImage = await ImagePicker.launchImageLibraryAsync({
+        allowsEditing: true,
+        aspect: [1, 1],
+      });
+      if (capturedImage.assets) {
+        console.log(capturedImage.assets[0]);
+        processImage(capturedImage.assets[0].uri);
+      }
+    }
+  };
+
+  async function processImage(imgUri) {
+    const processedImage = await ImageManipulator.manipulateAsync(
+      imgUri,
+      [{ resize: { width: 400 } }],
+      { format: 'png' }
+    );
+    console.log(processedImage);
+    setImageUrl(processedImage.uri);
+  }
 
   return (
     <ScrollView>
@@ -162,6 +189,7 @@ const RegisterTab = () => {
             style={styles.image}
           />
           <Button title='Camera' onPress={getImageFromCamera} />
+          <Button title='Gallery' onPress={getImageFromGallery} />
         </View>
         <Input
           placeholder='Username'
--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -235,15 +235,18 @@ const Main = () => {
     dispatch(fetchComments());
   }, [dispatch]);
 
+  const showNetInfo = async () => {
+    const connectionInfo = await NetInfo.fetch();
+    Platform.OS === 'ios'
+      ? Alert.alert('Initial Network Connectivity Type:', connectionInfo.type)
+      : ToastAndroid.show(
+          'Initial Network Connectivity Type: ' + connectionInfo.type,
+          ToastAndroid.LONG
+        );
+  };
+
   useEffect(() => {
-    NetInfo.fetch().then((connectionInfo) => {
-      Platform.OS === 'ios'
-        ? Alert.alert('Initial Network Connectivity Type:', connectionInfo.type)
-        : ToastAndroid.show(
-            'Initial Network Connectivity Type: ' + connectionInfo.type,
-            ToastAndroid.LONG
-          );
-    });
+    showNetInfo();
 
     const unsubscribeNetInfo = NetInfo.addEventListener((connectionInfo) => {
       handleConnectivityChange(connectionInfo);
